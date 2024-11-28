33d0f9a3577e669d687e860f4943fa323948a3be
Dpk Mht <meepak@gmail.com>
2024-11-28 21:42:15 +1030

    'larahook'

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to Proviso</title>
+    <title>Welcome to Larahook</title>
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,6 @@
 <header class="bg-indigo-600 text-white py-6 shadow-md">
     <div class="container mx-auto text-center">
-        <h1 class="text-4xl font-bold">Welcome to Store-Requests, Server Edition!</h1>
+        <h1 class="text-4xl font-bold">Welcome to Larahook!</h1>
         <p class="text-lg mt-2">Effortlessly manage and submit your API requests</p>
     </div>
 </header>
